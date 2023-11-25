9f928ef3463cd49b3d9f51d16615248a3a0f0321
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-25 02:19:59 -0500

    style home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className='flex flex-col flex-wrap justify-center m-auto text-center text-4xl h-screen'>
+    <div className='flex flex-col flex-wrap justify-center m-auto text-center text-4xl h-screen font-bold'>
       <h1>Home</h1>
     </div>
   )
